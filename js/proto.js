737ab6cd1a00f6b98578bb82e7dc0d2bf873f7c6
Daniel Lamando <dorm@danopia.net>
2013-07-23 21:24:04 -0400

    Use server timestamps

--- js/proto.js
+++ js/proto.js
@@ -4,8 +4,8 @@ var ops = {
   welcome: function (pkt, ex) {
     $.updateStatus(1, 'connected to ' + ex.server);
     
-    $.ui.log('debug', 'Received server welcome from ' + ex.server + ': ' + ex.software);
-    $.ui.log('debug', ex.server + ' is willing to use ' + ex.auth.join(', ') + ' for auth');
+    $.ui.log('debug', 'Received server welcome from ' + ex.server + ': ' + ex.software, pkt.ts);
+    $.ui.log('debug', ex.server + ' is willing to use ' + ex.auth.join(', ') + ' for auth', pkt.ts);
     
     if (ex.auth.indexOf('password') >= 0) {
       var user = prompt('username', window.localStorage['10bit.username']);
@@ -15,13 +15,13 @@ var ops = {
       
       $.send({op: 'auth', ex: {method: 'password', username: user, password: pass}});
     } else {
-      $.ui.log('status', 'Unable to authenticate. I don\'t know any methods that the server will accept :(');
+      $.ui.log('status', 'Unable to authenticate. I don\'t know any methods that the server will accept :(', pkt.ts);
     };
   },
   
   auth: function (pkt, ex) {
     if (ex.isack) {
-      $.ui.log('debug', 'Authentication as ' + ex.username + ' successful.');
+      $.ui.log('debug', 'Authentication as ' + ex.username + ' successful.', pkt.ts);
       $.updateStatus(2, 'logged in as ' + ex.username);
       $.user = ex.username;
     }
@@ -38,14 +38,14 @@ var ops = {
     if (pkt.ex && pkt.ex.isack) roomids.push(pkt.rm);
     
     $.ui.addNick(pkt.rm, pkt.sr);
-    $.ui.log(pkt.rm, pkt.sr + ' joined');
+    $.ui.log(pkt.rm, pkt.sr + ' joined', pkt.ts);
   },
   
   leave: function (pkt, ex) {
     if (!pkt.rm) return;
     if (pkt.ex && pkt.ex.isack) roomids.splice(roomids.indexOf(pkt.rm), 1);
     
-    $.ui.log(pkt.rm, pkt.sr + ' left');
+    $.ui.log(pkt.rm, pkt.sr + ' left', pkt.ts;
     $.ui.removeNick(pkt.rm, pkt.sr);
   },
   
@@ -54,7 +54,7 @@ var ops = {
       var id = roomids[idx];
       
       if ($.ui.getPage(id).meta.users.indexOf(pkt.sr) >= 0) {
-        $.ui.log(id, pkt.sr + ' disconnected');
+        $.ui.log(id, pkt.sr + ' disconnected', pkt.ts);
         $.ui.removeNick(id, pkt.sr);
       };
     };
@@ -70,23 +70,23 @@ var ops = {
     }
     
     if (ex.isaction)
-      $.ui.log(pkt.rm, '* ' + pkt.sr + ' ' + ex.message);
+      $.ui.log(pkt.rm, '* ' + pkt.sr + ' ' + ex.message, pkt.ts);
     else
-      $.ui.log(pkt.rm, '<' + pkt.sr + '> ' + ex.message);
+      $.ui.log(pkt.rm, '<' + pkt.sr + '> ' + ex.message, pkt.ts);
   }
 };
 
 $.gotPkt = function (pkt) {
-  $.ui.log('raw', '<<< ' + JSON.stringify(pkt));
+  $.ui.log('raw', '<<< ' + JSON.stringify(pkt), pkt.ts);
   
   if (ops[pkt.op])
     ops[pkt.op](pkt, pkt.ex);
   else
-    $.ui.log('debug', 'unhandled op', pkt.op, 'in', pkt);
+    $.ui.log('debug', 'unhandled op', pkt.op, 'in', pkt, pkt.ts);
 };
 
 $.send = function (pkt) {
-  $.ui.log('raw', '>>> ' + JSON.stringify(pkt));
+  $.ui.log('raw', '>>> ' + JSON.stringify(pkt), pkt.ts);
   
   $.socket.send(JSON.stringify(pkt));
 };
